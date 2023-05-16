e931ff322b3b582a9519974cd5a88f0d15048571
Trung <nguyentrung250302@gmail.com>
2023-05-16 11:04:05 +0700

    commit tap coming soon

--- src/BuildTap/ContactUsTap/ContactUsTap.vue
+++ src/BuildTap/ContactUsTap/ContactUsTap.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <header-view />
+    <!-- title -->
+    <div>
+      <h1>contact us</h1>
+    </div>
     <!-- body -->
     <div id="contact-us">
-
+      <div class="left">
+        <h1>left</h1>
+      </div>
+      <div class="right">
+        <h1>right</h1>
+      </div>
     </div>
     <!--  -->
     <footer-view />
@@ -14,16 +23,28 @@
 import HeaderView from "@/components/HeaderView.vue";
 import FooterView from "@/components/FooterView.vue";
 export default {
-    components: {
-        HeaderView,
-        FooterView,
-    }
-}
+  components: {
+    HeaderView,
+    FooterView,
+  },
+};
 </script>
 
 <style scoped>
 #contact-us {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+  width: 100%;
+  height: 500px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.left {
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.right {
+display: flex;
+justify-content: center;
+align-items: center;
 }
 </style>
